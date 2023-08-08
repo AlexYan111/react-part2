43ee817b11f99acfba3608b3174138f6cea4303d
Alex <wandong_yan@brown.edu>
2023-08-07 23:11:38 -0400

    Creating usePosts hook and fetching data

--- src/main.tsx
+++ src/main.tsx
@@ -7,18 +7,7 @@ import App from "./App";
 import "./index.css";
 
 // Global Query Settings
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 3, // how many times to retry on errors
-      cacheTime: 300000, // 5 minutes, garbage collection
-      staleTime: 10 * 1000, // 10s, how long the data is considered fresh, default is 0
-      refetchOnWindowFocus: false, // refetch data when window regains focus, but default is true
-      refetchOnMount: false, // refetch data when component mounts, but default is true
-      refetchOnReconnect: false, // refetch data when network reconnects, but default is true
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
