d5f9dfd6d53b105921843b1fe80e4addf269a1ea
Alex <wandong_yan@brown.edu>
2023-08-09 23:05:44 -0400

    Implementing infinite queries

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { Query, useQuery } from "@tanstack/react-query";
+import { Query, useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -9,26 +9,29 @@ interface Post {
 }
 
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
 const usePosts = (query: PostQuery) => {
-  const fetchPosts = () =>
+  const fetchPosts = ({pageParam = 1}) =>
     axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
         params: { 
-          _start: (query.page - 1) * query.pageSize, // _start is the index of the first item to return
+          _start: (pageParam - 1) * query.pageSize, // _start is the index of the first item to return
           _limit: query.pageSize,
          },
       })
       .then((res) => res.data);
  
-  return useQuery<Post[], Error>({
+  return useInfiniteQuery<Post[], Error>({
     queryKey: ["posts", query],
     queryFn: fetchPosts,
     staleTime: 60 * 1000, // 1m, Per query Query Setting
     keepPreviousData: true, // this will keep the previous data when the query is invalidated
+    getNextPageParam: (lastPage, allPages) => {
+      // 1->2
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    }
   });
 }
 
