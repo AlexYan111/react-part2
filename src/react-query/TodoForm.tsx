e53a742a43fadd8fbb23490ae5486e51647cd8ae
Alex <wandong_yan@brown.edu>
2023-08-11 21:25:51 -0400

    Creating a Custom Mutation Hook

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,49 +1,13 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
 import { useRef } from "react";
-import { Todo } from "./hooks/useTodos";
-
-interface AddTodoContest {
-  previousTodos: Todo[];
-}
+import useAddTodo from "./hooks/useAddTodo";
 
 const TodoForm = () => {
-  const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo, Error, Todo, AddTodoContest>({
-    mutationFn: (todo: Todo) =>
-      axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/todosx", todo)
-        .then((res) => res.data),
-
-    onMutate: (newTodo: Todo) => {
-      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        newTodo,
-        ...(todos || []),
-      ]);
-
-      if (ref.current) {
-        ref.current.value = "";
-      }
-
-      return { previousTodos };
-    },
-
-    // savedTodo is the result from backend, newTodo is the data we passed to onMutate
-    onSuccess: (savedTodo, newTodo) => {
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
-        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
-      );
-    },
-
-    onError: (err, newTodo, context) => {
-      if (!context) return;
-
-      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
-    },
-  });
-
   const ref = useRef<HTMLInputElement>(null);
+  const addTodo = useAddTodo(() => {
+    if (ref.current) {
+      ref.current.value = "";
+    }
+  });
 
   return (
     <>
