257c4ca9ff3369ad550c21dc42927e90355a2924
Alex <wandong_yan@brown.edu>
2023-08-14 20:53:46 -0400

    Managing application state with Zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Counter from "./state-management/counter/Counter";
 import HomePage from "./state-management/HomePage";
 import LoginProvider from "./state-management/login/LoginProvider";
 import NavBar from "./state-management/NavBar";
@@ -9,6 +10,7 @@ function App() {
     <>
       <LoginProvider>
         <TasksProvider>
+          <Counter />
           <NavBar />
           <HomePage />
         </TasksProvider>
