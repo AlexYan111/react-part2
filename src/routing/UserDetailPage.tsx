80b1493377aa517277857575db3eaabfa8f4cb10
Alex <wandong_yan@brown.edu>
2023-08-15 21:56:54 -0400

    Building navbar using nested routes

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,11 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const pramas = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  console.log(location);
+
   return <p>User</p>;
 };
 
